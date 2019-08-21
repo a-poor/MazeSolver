52a8fba3ce718ff26ce027b38b91b1c9c21d146a
a-poor <45295232+a-poor@users.noreply.github.com>
2019-08-20 23:07:44 -0400

    Update sketch.js
    Connecting sketch to the buttons

--- sketch.js
+++ sketch.js
@@ -1,14 +1,27 @@
-let n_cols = 20;
-let n_rows = 20;
+let n_cols = 30;
+let n_rows = 30;
 let sq_width;
-let maze_maker;
-let solver;
-let solver_built = false;
+let maze_maker, solver;
+let solve_maze = false;
+
+function start_solve() {
+    if (maze_maker.n_unvisited <= 0 && !solve_maze) {
+        solve_maze = true;
+        solver = new AStar(maze_maker.grid, sq_width);
+    }
+}
+function reset_grid() {
+    solver = undefined;
+    solve_maze = false;
+    document.getElementById("solve-button").disabled = true;
+    maze_maker = new MazeMaker(n_cols, n_rows, sq_width);
+    loop();
+}
 
 function setup() {
     let cnv = createCanvas(600,600);
     cnv.parent("sketch-holder");
-    cnv.position((windowWidth - width) / 2);
+    document.getElementById("solve-button").disabled = true;
 
     sq_width = width / n_cols;
     maze_maker = new MazeMaker(n_cols, n_rows, sq_width);
@@ -18,13 +31,12 @@ function draw() {
     background(40);
     if (maze_maker.n_unvisited > 0) {
         maze_maker.get_next_move();
-        maze_maker.show();
     } else {
-        if (!solver_built) {
-            solver = new AStar(maze_maker.grid, sq_width);
-            solver_built = true;
-        }
-        maze_maker.show();
+        document.getElementById("solve-button").disabled = false;
+        maze_built = true;
+    }
+    maze_maker.show();
+    if (solve_maze) {
         solver.get_next_move();
     }
 }
